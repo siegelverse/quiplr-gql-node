b5a7e460be548468002d1a7706702653b3bd275c
siegelverse <msiegel37@gmail.com>
2021-04-20 15:00:16 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import { ApolloServer, gql } from 'apollo-server-express';
+import express from 'express';
+import mongoose from 'mongoose';
+import { typeDefs, resolvers } from './schema';
+
+const app = express()
+
+const server = new ApolloServer({
+    typeDefs, 
+    resolvers,
+})
+
+server.applyMiddleware({ app })
+
+app.listen({ port: 4000 } , () => 
+    console.log(`🚀 Server ready at: http://localhost:4000${server.graphqlPath}`)
+)
